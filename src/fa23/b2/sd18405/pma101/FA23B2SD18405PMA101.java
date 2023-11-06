0b8af9c96babc033ce5dd7fba92b40c45b9c1de1
Your Name <you@example.com>
2023-11-06 22:34:56 +0700

    init project

---
+++ src/fa23/b2/sd18405/pma101/FA23B2SD18405PMA101.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package fa23.b2.sd18405.pma101;
+
+/**
+ *
+ * @author KhanhCT
+ */
+public class FA23B2SD18405PMA101 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
